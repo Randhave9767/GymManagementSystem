3a6b9a9eace79dc8077cc859a42e43f91ef9fe32
Nandinee Gupta <nandinee_gupta@persistent.com>
2023-03-07 11:24:48 +0530

    incomming

---
+++ GymManagementSystem/Models/Equipment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GymManagementSystem.Models;
+
+public partial class Equipment
+{
+    public int EquipmentId { get; set; }
+
+    public string? EquipmentName { get; set; }
+
+    public int? Quantity { get; set; }
+}
