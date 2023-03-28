de6f9b8db993f6691693baeaff4f07617332e7c8
Nandinee Gupta <nandinee_gupta@persistent.com>
2023-03-28 10:52:35 +0530

    ui

--- GymManagementSystem/Models/GymDatabaseContext.cs
+++ GymManagementSystem/Models/GymDatabaseContext.cs
@@ -23,12 +23,6 @@ public partial class GymDatabaseContext : DbContext
 
     public virtual DbSet<MembershipType> MembershipTypes { get; set; }
 
-    public virtual DbSet<Table1> Table1s { get; set; }
-
-    public virtual DbSet<Table2> Table2s { get; set; }
-
-    public virtual DbSet<Table3> Table3s { get; set; }
-
     public virtual DbSet<Trainer> Trainers { get; set; }
 
     public virtual DbSet<User> Users { get; set; }
@@ -37,29 +31,29 @@ public partial class GymDatabaseContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=PSL-G357XM3;Database=GymDatabase;Trusted_Connection=True;Encrypt=false;");
+        => optionsBuilder.UseSqlServer("Server=PSL-6957XM3;Database=GymDatabase;Trusted_Connection=True;Encrypt=false;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Equipment>(entity =>
         {
-            entity.HasKey(e => e.EquipmentId).HasName("PK__Equipmen__197068AFBC722DE9");
+            entity.HasKey(e => e.EquipmentId).HasName("PK__Equipmen__197068AF5F5D0FCE");
 
             entity.Property(e => e.EquipmentId).HasColumnName("equipment_id");
             entity.Property(e => e.EquipmentName)
-                .HasMaxLength(20)
+                .HasMaxLength(200)
                 .HasColumnName("equipment_name");
             entity.Property(e => e.Quantity).HasColumnName("quantity");
         });
 
         modelBuilder.Entity<EquipmentRequest>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__equipmen__3214EC076F265E16");
+            entity.HasKey(e => e.Id).HasName("PK__equipmen__3214EC07B8748A1F");
 
             entity.ToTable("equipment_requests");
 
             entity.Property(e => e.EquipmentName)
-                .HasMaxLength(20)
+                .HasMaxLength(100)
                 .IsUnicode(false)
                 .HasColumnName("equipment_name");
             entity.Property(e => e.Quantity).HasColumnName("quantity");
@@ -68,19 +62,19 @@ public partial class GymDatabaseContext : DbContext
                 .IsUnicode(false)
                 .HasColumnName("reason");
             entity.Property(e => e.RequestStatus)
-                .HasMaxLength(20)
+                .HasMaxLength(100)
                 .IsUnicode(false)
                 .HasColumnName("request_status");
             entity.Property(e => e.TrainerId).HasColumnName("trainer_id");
 
             entity.HasOne(d => d.Trainer).WithMany(p => p.EquipmentRequests)
                 .HasForeignKey(d => d.TrainerId)
-                .HasConstraintName("FK__equipment__train__4E88ABD4");
+                .HasConstraintName("FK__equipment__train__4F7CD00D");
         });
 
         modelBuilder.Entity<Member>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Member__3214EC077A4B1585");
+            entity.HasKey(e => e.Id).HasName("PK__Member__3214EC07A7C84C75");
 
             entity.ToTable("Member");
 
@@ -96,20 +90,20 @@ public partial class GymDatabaseContext : DbContext
 
             entity.HasOne(d => d.Membership).WithMany(p => p.Members)
                 .HasForeignKey(d => d.MembershipId)
-                .HasConstraintName("FK__Member__membersh__2E1BDC42");
+                .HasConstraintName("FK__Member__membersh__2F10007B");
 
             entity.HasOne(d => d.Trainer).WithMany(p => p.Members)
                 .HasForeignKey(d => d.TrainerId)
-                .HasConstraintName("FK__Member__trainer___2D27B809");
+                .HasConstraintName("FK__Member__trainer___2E1BDC42");
 
             entity.HasOne(d => d.User).WithMany(p => p.Members)
                 .HasForeignKey(d => d.UserId)
-                .HasConstraintName("FK__Member__user_id__2C3393D0");
+                .HasConstraintName("FK__Member__user_id__2D27B809");
         });
 
         modelBuilder.Entity<MembershipType>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__membersh__3214EC077EA8ACB8");
+            entity.HasKey(e => e.Id).HasName("PK__membersh__3214EC07EB772344");
 
             entity.ToTable("membership_types");
 
@@ -124,47 +118,9 @@ public partial class GymDatabaseContext : DbContext
                 .HasColumnName("membership_name");
         });
 
-        modelBuilder.Entity<Table1>(entity =>
-        {
-            entity
-                .HasNoKey()
-                .ToTable("Table_1");
-
-            entity.Property(e => e.Id)
-                .HasMaxLength(10)
-                .IsFixedLength()
-                .HasColumnName("id");
-        });
-
-        modelBuilder.Entity<Table2>(entity =>
-        {
-            entity.ToTable("Table_2");
-
-            entity.Property(e => e.Id).HasColumnName("id");
-            entity.Property(e => e.JoiningDate)
-                .HasColumnType("date")
-                .HasColumnName("joining_date");
-            entity.Property(e => e.UserId)
-                .HasMaxLength(10)
-                .IsFixedLength()
-                .HasColumnName("user_id");
-        });
-
-        modelBuilder.Entity<Table3>(entity =>
-        {
-            entity
-                .HasNoKey()
-                .ToTable("Table_3");
-
-            entity.Property(e => e.Id)
-                .ValueGeneratedOnAdd()
-                .HasColumnName("id");
-            entity.Property(e => e.UserId).HasColumnName("userId");
-        });
-
         modelBuilder.Entity<Trainer>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__trainer__3214EC07E05630FE");
+            entity.HasKey(e => e.Id).HasName("PK__trainer__3214EC070F25D71A");
 
             entity.ToTable("trainer");
 
@@ -178,12 +134,12 @@ public partial class GymDatabaseContext : DbContext
 
             entity.HasOne(d => d.User).WithMany(p => p.Trainers)
                 .HasForeignKey(d => d.UserId)
-                .HasConstraintName("FK__trainer__user_id__276EDEB3");
+                .HasConstraintName("FK__trainer__user_id__286302EC");
         });
 
         modelBuilder.Entity<User>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__User__3214EC0762A373B1");
+            entity.HasKey(e => e.Id).HasName("PK__User__3214EC07E27513D2");
 
             entity.ToTable("User");
 
@@ -191,7 +147,7 @@ public partial class GymDatabaseContext : DbContext
                 .HasMaxLength(100)
                 .HasColumnName("full_name");
             entity.Property(e => e.MobileNo)
-                .HasMaxLength(50)
+                .HasMaxLength(15)
                 .HasColumnName("mobile_no");
             entity.Property(e => e.Password)
                 .HasMaxLength(50)
@@ -203,7 +159,7 @@ public partial class GymDatabaseContext : DbContext
 
         modelBuilder.Entity<Userfeedback>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Userfeed__3213E83FFB922158");
+            entity.HasKey(e => e.Id).HasName("PK__Userfeed__3213E83F06F164D9");
 
             entity.ToTable("Userfeedback");
 
@@ -221,7 +177,7 @@ public partial class GymDatabaseContext : DbContext
 
             entity.HasOne(d => d.User).WithMany(p => p.Userfeedbacks)
                 .HasForeignKey(d => d.UserId)
-                .HasConstraintName("FK__Userfeedb__userI__49C3F6B7");
+                .HasConstraintName("FK__Userfeedb__userI__36B12243");
         });
 
         OnModelCreatingPartial(modelBuilder);
